efb7a479bca63f78b3043ae3cac23fa8b7774cab
django-github <oz5java@yahoo.co.jp>
2021-09-30 16:11:54 +0900

    Change Dictionary recording

--- account/views.py
+++ account/views.py
@@ -40,9 +40,19 @@ class Top(generic.TemplateView):
                 verb_list = excel_list[1]
                 #dictionary = Dictionary.objects.all()
                 #dictionary.delete()
-                for i in range(len(verb_list)):
-                    dictionary = Dictionary(item=verb_list[i], category= verb_dic[verb_list[i]]["category"], japanese= verb_dic[verb_list[i]]["japanese"], english= verb_dic[verb_list[i]]["english"], esound= "sancom_free/sound/" + verb_dic[verb_list[i]]["esound"], chinese= verb_dic[verb_list[i]]["chinese"], csound= "sancom_free/sound/" + verb_dic[verb_list[i]]["csound"])
-                    dictionary.save()
+                #for i in range(len(verb_list)):
+                    #dictionary=Dictionary()
+                    #dictionary.item=verb_list[i]
+                    #dictionary.category=verb_dic[verb_list[i]]["category"]
+                    #dictionary.japanese=verb_dic[verb_list[i]]["japanese"]
+                    #dictionary.english= verb_dic[verb_list[i]]["english"]
+                    #dictionary.esound= "sancom_free/sound/" + verb_dic[verb_list[i]]["esound"]
+                    #dictionary.chinese= verb_dic[verb_list[i]]["chinese"]
+                    #dictionary.csound= "sancom_free/sound/" + verb_dic[verb_list[i]]["csound"]
+                    #dictionary.save()
+                    #dictionary = Dictionary.objects.create(item=verb_list[i], category= verb_dic[verb_list[i]]["category"], japanese= verb_dic[verb_list[i]]["japanese"], english= verb_dic[verb_list[i]]["english"], esound= "sancom_free/sound/" + verb_dic[verb_list[i]]["esound"], chinese= verb_dic[verb_list[i]]["chinese"], csound= "sancom_free/sound/" + verb_dic[verb_list[i]]["csound"])
+                    #dictionary = Dictionary(item=verb_list[i], category= verb_dic[verb_list[i]]["category"], japanese= verb_dic[verb_list[i]]["japanese"], english= verb_dic[verb_list[i]]["english"], esound= "sancom_free/sound/" + verb_dic[verb_list[i]]["esound"], chinese= verb_dic[verb_list[i]]["chinese"], csound= "sancom_free/sound/" + verb_dic[verb_list[i]]["csound"])
+                    #dictionary.save()
 
             self.params['visiter'] = user_name
         return render(self.request,'index.html', self.params)
